523d0f439eba344fe2b1cfd7e4ddc24bc90d6a30
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-09-29 11:22:20 +0700

    update backend 29 sep 23

---
+++ src/conrtollers/auth-controller.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcryptjs");
+const { registerSchema, loginSchema } = require("../validators/auth-validator");
+const jwt = require("jsonwebtoken");
+const prisma = require("../models/prisma");
+const createError = require("../utils/create-error");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { value, error } = registerSchema.validate(req.body);
+    if (error) {
+      return next(error);
+    }
+    value.password = await bcrypt.hash(value.password, 12);
+    const user = await prisma.user.create({
+      data: value,
+    });
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || "12dasdsfgewrwasdf",
+      { expiresIn: process.env.JWT_EXPIRE }
+    );
+    res.status(201).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { value, error } = loginSchema.validate(req.body);
+    if (error) {
+      return next(error);
+    }
+    // only 1 row in database use findFirst
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: value.emailOrMobile }, { mobile: value.emailOrMobile }],
+      },
+    });
+    if (!user) {
+      return next(createError("invalid credential", 400));
+    }
+    const isMatch = await bcrypt.compare(value.password, user.password);
+    if (!isMatch) {
+      return next(createError("invalid credential", 400));
+    }
+    const payload = { userId: user.id };
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || "12dasdsfgewrwasdf",
+      { expiresIn: process.env.JWT_EXPIRE }
+    );
+    res.status(201).json({ accessToken });
+  } catch (err) {
+    next(err);
+  }
+};
